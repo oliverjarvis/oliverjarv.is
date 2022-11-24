384125680918d1af218551016587bf07a62193ed
Oliver Jarvis <oliversimonjarvis@gmail.com>
2022-11-24 11:35:05 +0100

    added prelim blog functionality

--- pages/blog/[id].js
+++ pages/blog/[id].js
@@ -29,7 +29,7 @@ export async function getStaticPaths(){
       });
 
     return {
-        paths: [{ params: { id: '1' } }, { params: { id: '2' } }],
+        paths: files,
         fallback: "blocking", // can also be true or 'blocking'
     }
 }
